b1dff4e81d2c9eb5c1a86ff1cc275b0029000a43
John Mark <johomurk@gmail.com>
2023-08-25 21:37:45 +0000

    ignore certs on host

--- echo-chamber/src/index.js
+++ echo-chamber/src/index.js
@@ -1,37 +1,61 @@
 import fs from 'fs';
 import http2 from 'http2';
-import fastify from 'fastify';
 import { Observable } from 'rxjs';
+import Fastify from 'fastify';
+const app = Fastify({
+  logger: true
+});
 
-// export function createGqlObservable(operation) {
-//   return new Observable(subscriber => {});
-// }
 
-const app = fastify({
-  http2: true  // Enable HTTP/2
+// Route for serving SSEs
+app.get('/sse', (request, reply) => {
+  console.log("--------------Client connected--------------------");
+  // Set appropriate headers for SSEs
+  reply.header('Content-Type', 'text/event-stream');
+  reply.header('Cache-Control', 'no-cache');
+  reply.header('Connection', 'keep-alive');
+  reply.header('X-Accel-Buffering', 'no'); // Disable buffering for Nginx
+
+  // Send initial data
+  reply.send(':ok\n\n');
+
+  // Simulate sending SSEs periodically
+  const intervalId = setInterval(() => {
+    const eventData = {
+      event: 'message',
+      data: `Message from server at ${new Date().toISOString()}`
+    };
+
+    reply.send(`event: ${eventData.event}\ndata: ${eventData.data}\n\n`);
+  }, 5000); // Send data every 2 seconds
+
+  // Clean up when the client closes the connection
+  request.raw.on('close', () => {
+    console.log("--------------Client closed connection--------------------");
+    clearInterval(intervalId);
+  });
 });
 
 // Load SSL certificate and key
 const options = {
-  key: fs.readFileSync('./private-key.pem'),
-  cert: fs.readFileSync('./certificate.pem')
+  key: fs.readFileSync('./localhost.direct.key'),
+  cert: fs.readFileSync('./localhost.direct.crt')
 };
 
-// Define your routes
-app.get('/', (request, reply) => {
-  console.log('Request received:', request.headers);
-  reply.type('text/html').send('<h1>Hello, HTTP/2 Fastify Server!</h1>');
+// Create an HTTP/2 server using the provided SSL certificate and private key
+const server = http2.createSecureServer(options, app);
+
+// Listen for the 'listening' event to handle server startup
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`Server listening on ${address.address}:${address.port}`);
 });
-app.get('/hey', (request, reply) => {
-  reply.type('text/html').send('<h1>Hello, HTTP/2 Fastify Server!</h1>');
+
+// Listen for the 'error' event to handle startup errors
+server.on('error', (err) => {
+  console.error('Error starting server:', err);
+  process.exit(1);
 });
 
 // Start the server
-const server = http2.createSecureServer(options, app);
-server.listen(3000, (err) => {
-  if (err) {
-    console.error('Error starting server:', err);
-    process.exit(1);
-  }
-  console.log('Server listening on port 3000');
-});
+server.listen(3000);
