bbeff7b9a314a7b1d7e92ba808b786a90a9cf638
John Mark Redding <johomurk@gmail.com>
2023-09-03 12:35:06 -0500

    Setup server sent events, handle OPTIONS request

--- echo-chamber/src/index.js
+++ echo-chamber/src/index.js
@@ -1,6 +1,21 @@
 import fs from 'fs';
 import http2 from 'http2';
 
+// --------------------------REMOVE THIS--------------------------
+// --------------------------REMOVE THIS--------------------------
+// --------------------------REMOVE THIS--------------------------
+const getDummyMessage = () => {
+  return { text: 'Help from server!', id: Math.random(), coords: {latitude: Math.random(), longitude: Math.random()}};
+}
+const messages = [
+  getDummyMessage(),
+  getDummyMessage(),
+  getDummyMessage()
+];
+// --------------------------REMOVE THIS--------------------------
+// --------------------------REMOVE THIS--------------------------
+// --------------------------REMOVE THIS--------------------------
+
 const PORT = process.env.PORT || 8443;
 const HOST = process.env.HOST || 'localhost';
 
@@ -17,7 +32,42 @@ server.on('error', (err) => {
 });
 
 server.on('stream', (stream, headers) => {
-  if (headers[":path"] === '/events') {
+  if (headers[":method"] === 'OPTIONS') {
+    stream.respond({
+      ':status': 204, // No content
+      'access-control-allow-origin': 'http://localhost:3000',
+      'access-control-allow-methods': 'POST, GET, OPTIONS, PATCH, DELETE',
+      'access-control-allow-headers': 'content-type'
+    });
+  } else if (headers[":path"] === '/echo' && headers[":method"] === 'POST') {
+      let requestBody = ''; // To store the incoming data
+
+    // Read incoming data from the client
+    stream.on('data', (chunk) => {
+      requestBody += chunk;
+    });
+
+    // When all data is received
+    stream.on('end', () => {
+      try {
+        const jsonData = JSON.parse(requestBody); // Assuming the data is in JSON format
+        const newEcho = {...jsonData.data, id: Math.random()};
+        messages.push(newEcho);
+        // Respond to the client
+        stream.respond({
+          ':status': 200,
+          'content-type': 'application/json',
+          'cache-control': 'no-cache',
+          'access-control-allow-origin': 'http://localhost:3000',
+        });
+        stream.end(JSON.stringify({data: newEcho})); // Echo the data back
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+        stream.respond({ ':status': 400 }); // Bad request
+        stream.end('Bad Request');
+      }
+    });
+  } else if (headers[":path"] === '/events') {
     // Set headers for SSE
     stream.respond({
       ':status': 200,
@@ -26,13 +76,9 @@ server.on('stream', (stream, headers) => {
       'access-control-allow-origin': '*',
     });
 
-    // Send initial message
-    const initialMessage = 'data: Connected\n\n';
-    stream.write(initialMessage);
-
     // Send SSE at regular intervals
     const intervalId = setInterval(() => {
-      const event = `data: ${JSON.stringify({ message: 'Hello from server!' })}\n\n`;
+      const event = `data: ${JSON.stringify(messages)}\n\n`;
       stream.write(event);
     }, 1000);
 
