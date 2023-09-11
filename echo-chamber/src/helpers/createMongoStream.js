f2c4ea3054ffe09c61392dfc73c889c5d5949a16
John Mark Redding <johomurk@gmail.com>
2023-09-11 10:16:32 -0500

    Use shared mongoclient

--- echo-chamber/src/helpers/createMongoStream.js
+++ echo-chamber/src/helpers/createMongoStream.js
@@ -1,16 +1,14 @@
 'use strict';
 import { Observable } from 'rxjs';
-import { MongoClient } from 'mongodb';
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
-export default ({uri, dbName, collectionName, pipelineFilter, ...mongoOptions}) => {
+export default (args) => {
+  const {client, dbName, collectionName, pipelineFilter} = args;
   return new Observable((subscriber) => {
-    const client = new MongoClient(uri, mongoOptions);
     const echoChanges = client
       .db(dbName)
       .collection(collectionName)
       .watch(pipelineFilter);
-
+      
     echoChanges.on("change", (e) => subscriber.next(e));
     echoChanges.on("error", (err) => subscriber.error(err));
     echoChanges.on("close", (e) => subscriber.complete(e));
