1f099d8271d7c5d0273a601bd9e88f1790819644
John Mark Redding <johomurk@gmail.com>
2023-09-12 14:11:46 -0500

    DRY up headers by hoisting

--- echo-chamber/src/helpers/sendServerEvent.js
+++ echo-chamber/src/helpers/sendServerEvent.js
@@ -1,21 +1,16 @@
 export default function sendServerEvent (data, eventName = 'message') {
   try {
+    const headers = {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      'Access-Control-Allow-Origin': '*'
+    };
     if (!this.raw.headersSent) {
       this.code(200);
-      this.type('text/event-stream');
-      this.headers({
-        'Cache-Control': 'no-cache',
-        'Access-Control-Allow-Origin': '*'
-      });
+      this.headers(headers);
       /*Server sent events don't seem to like the fastify abstraction, 
       so we need to use the raw response. */
-      this.raw.writeHead(
-        200, {
-          'Content-Type': 'text/event-stream',
-          'Cache-Control': 'no-cache',
-          'Access-Control-Allow-Origin': '*'
-        }
-      );
+      this.raw.writeHead(200, headers);
     }
     this.raw.write(`event: ${eventName}\ndata: ${JSON.stringify(data)}\n\n`);
   } catch (sseError) {
