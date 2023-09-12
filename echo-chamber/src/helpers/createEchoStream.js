6fbdba259fea9b51723aabfa5d750263d47cb147
John Mark Redding <johomurk@gmail.com>
2023-09-12 12:41:32 -0500

    Setup echoStream to start with fetched echoes

--- echo-chamber/src/helpers/createEchoStream.js
+++ echo-chamber/src/helpers/createEchoStream.js
@@ -29,13 +29,20 @@ export default ({latitude,longitude}) => {
   const collection = useMongoClient(DB_NAME, DB_COLLECTION_NAME);
   const mongoStream = createMongoStream({collection, pipelineFilter})
     .pipe(
-      startWith([])
+      startWith(getEchoes(collection, {latitude, longitude}))
     );
   // Grab updated echoes when mongo stream alerts
   const echoStream = new Observable((subscriber) => {
     const mongoSubscription = mongoStream.subscribe({
-      next: (e) => { subscriber.next(getEchoes({latitude,longitude})) },
-      error: (mongoSubError) => { console.error(mongoSubError) },
+      next: (e) => {
+        getEchoes(collection, {latitude, longitude})
+          .then((fetchedEchoes) => {
+            subscriber.next(fetchedEchoes);
+            return fetchedEchoes;
+          })
+          .catch((err) => console.error(err));
+      },
+      error: (mongoSubError) => { subscriber.error(mongoSubError) },
       complete: (e) => { subscriber.complete(e) }
     })
     return () => {
