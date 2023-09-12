8dce100bf75e14954adc809b209d6651165dec39
John Mark Redding <johomurk@gmail.com>
2023-09-11 21:13:43 -0500

    Build out insertEcho.js for posting to DB.

--- echo-chamber/src/helpers/insertEcho.js
+++ echo-chamber/src/helpers/insertEcho.js
@@ -1,3 +1,27 @@
-export default (data) => {
-    return {...data, id: Math.random()}
+'use strict'
+export default async ({collection, data}) => {
+  const dbFormattedEcho = {
+    text: data.text,
+    location: {
+      type: "Point",
+      coordinates: [ data.coords.latitude, data.coords.longitude ]
+    }
+  };
+  try {
+    const inserted = await collection.insertOne(dbFormattedEcho);
+    const { _id, text, location } = await collection.findOne(inserted.insertedId);
+    
+    const clientFormattedEcho = {
+      id: _id.toString(),
+      text: text,
+      coords: {
+        latitude: location.coordinates[0],
+        longitude: location.coordinates[1]
+      }
+    };
+
+    return clientFormattedEcho;
+  } catch (err) {
+    console.error(err);
+  }
 };
