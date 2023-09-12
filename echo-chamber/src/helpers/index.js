8292302baeac497e9480196621834ceacd69f56b
John Mark Redding <johomurk@gmail.com>
2023-09-12 12:45:22 -0500

    Export toClientFormattedEcho from helpers

--- echo-chamber/src/helpers/index.js
+++ echo-chamber/src/helpers/index.js
@@ -4,3 +4,4 @@ export {default as sendServerEvent} from "./sendServerEvent.js";
 export {default as createEchoStream} from "./createEchoStream.js";
 export {default as createMongoStream} from "./createMongoStream.js";
 export {default as useMongoClient} from "./useMongoClient.js";
+export {default as toClientFormattedEcho} from "./toClientFormattedEcho.js";
