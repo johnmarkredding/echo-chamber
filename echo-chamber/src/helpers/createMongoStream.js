b822e5b416108548e5728979dd649e31dd12fc9f
John Mark Redding <johomurk@gmail.com>
2023-09-14 10:45:44 -0500

    Reformat callbacks

--- echo-chamber/src/helpers/createMongoStream.js
+++ echo-chamber/src/helpers/createMongoStream.js
@@ -5,13 +5,10 @@ export default (args) => {
   const { collection, pipelineFilter } = args;
   const newMongoStream = new Observable((subscriber) => {
     const echoChanges = collection.watch(pipelineFilter);
-      
-    echoChanges.on("change", (e) => subscriber.next(e));
-    echoChanges.on("error", (err) => subscriber.error(err));
-    echoChanges.on("close", (closeEvent) => subscriber.complete(closeEvent));
-    return () => {
-      echoChanges.close();
-    }
+    echoChanges.on("change", (e) => { subscriber.next(e) });
+    echoChanges.on("error", (err) => { subscriber.error(err) });
+    echoChanges.on("close", (closeEvent) => { subscriber.complete(closeEvent) });
+    return () => { echoChanges.close() }
   });
   return newMongoStream.pipe(startWith("Initial event to load data"));
 };
