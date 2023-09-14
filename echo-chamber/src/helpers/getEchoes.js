318888ec655d67e4d6dcbb5573d34dc2814f46eb
John Mark Redding <johomurk@gmail.com>
2023-09-14 10:47:05 -0500

    Implement getEchoes mongodb query

--- echo-chamber/src/helpers/getEchoes.js
+++ echo-chamber/src/helpers/getEchoes.js
@@ -1,18 +1,16 @@
 import { toClientFormattedEcho } from "../helpers/index.js";
+import { metersToRadians } from "../utilities/index.js";
+const { QUERY_RADIUS_M } = process.env;
 
 export default (collection, {latitude, longitude}) => {
   return collection.find(
-  //   {
-  //   "location": {
-  //     $nearSphere: {
-  //       $geometry: {
-  //         type: "Point",
-  //         coordinates: [longitude, latitude]
-  //       },
-  //       $maxDistance: Number(process.env.QUERY_RADIUS)
-  //     }
-  //   }
-  // }
+    {
+    "location": {
+      $geoWithin: {
+        $centerSphere: [[longitude, latitude], metersToRadians(QUERY_RADIUS_M)]
+      }
+    }
+  }
   ).toArray()
     .then((locatedEchoes) => locatedEchoes.map(toClientFormattedEcho))
     .catch((err) => { console.error("Get Echoes Error", err) });
