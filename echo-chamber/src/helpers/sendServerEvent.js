a8082c006af21a9a7d1f677a32f89cf7efef0313
John Mark Redding <johomurk@gmail.com>
2023-09-14 21:44:30 -0500

    Specify encoding header, optimize header creation

--- echo-chamber/src/helpers/sendServerEvent.js
+++ echo-chamber/src/helpers/sendServerEvent.js
@@ -1,18 +1,20 @@
-export default function sendServerEvent (data, eventName = 'message') {
+const sseHeaders = {
+  'Content-Type': 'text/event-stream; charset=utf-8',
+  'Cache-Control': 'no-cache',
+  'Access-Control-Allow-Origin': '*'
+};
+
+export default function sendServerEvent (data, eventName = "message") {
   try {
-    const headers = {
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      'Access-Control-Allow-Origin': '*'
-    };
+    // Can't set headers after sending the first message
     if (!this.raw.headersSent) {
       this.code(200);
-      this.headers(headers);
+      this.headers(sseHeaders);
       /*Server sent events don't seem to like the fastify abstraction, 
       so we need to use the raw response. */
-      this.raw.writeHead(200, headers);
+      this.raw.writeHead(200, sseHeaders);
     }
-    this.raw.write(`event: ${eventName}\ndata: ${JSON.stringify(data)}\n\n`);
+    this.raw.write(`event: ${eventName}\ndata: ${JSON.stringify(data || "No data")}\n\n`);
   } catch (sseError) {
     console.error("Error sending SSE:", sseError);
     this.code(500);
