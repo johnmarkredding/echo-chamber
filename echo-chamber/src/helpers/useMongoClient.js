5de9b6bca74a2debb913b099f657d3c6035e518e
John Mark Redding <johomurk@gmail.com>
2023-09-18 11:39:10 -0700

    Export different func depending on ping result

--- echo-chamber/src/helpers/useMongoClient.js
+++ echo-chamber/src/helpers/useMongoClient.js
@@ -9,21 +9,29 @@ const mongoOptions = {
   }
 };
 
+let useMongoClient = () => { console.log("MongoClient not initialized") };
+
 try {
   const echoMongoClient = new MongoClient(mongoUri, mongoOptions);
 
   // Test DB reachability
   echoMongoClient.db(process.env.DB_NAME)
     .command({ ping: 1 })
-    .then((dbRes) => { console.log(dbRes.ok ? "DB reachable!" : dbRes) })
+    .then((dbRes) => console.log(dbRes.ok ? "DB reachable!" : dbRes))
+    // then create TTL index
+    .then(() => {
+      echoMongoClient.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION_NAME).createIndex({ "timestamp": 1 }, { expireAfterSeconds: 3600 }).then(console.log);
+    })
     .catch(console.error);
+
+  // Set which function to export depending on success of the ping
+  useMongoClient = (dbName = null, collectionName = null) => {
+    const selectedDb = dbName ? echoMongoClient?.db(dbName) : echoMongoClient;
+    const selectedCollection = collectionName ? selectedDb?.collection(collectionName) : selectedDb;
+    return selectedCollection || {};
+  };
 } catch (mongoClientCreationError) {
   console.error(mongoClientCreationError);
 }
 
-
-export default (dbName = null, collectionName = null) => {
-  const selectedDb = dbName ? echoMongoClient?.db(dbName) : echoMongoClient;
-  const selectedCollection = collectionName ? selectedDb?.collection(collectionName) : selectedDb;
-  return selectedCollection || {};
-};
+export default useMongoClient;
