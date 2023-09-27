78185816e8f5a47c640dbe8075c95183a3568086
John Mark Redding <johomurk@gmail.com>
2023-09-27 14:32:35 -0700

    Clarify what happens with the time range filter

--- echo-chamber/src/helpers/getEchoes.js
+++ echo-chamber/src/helpers/getEchoes.js
@@ -3,9 +3,9 @@ import { metersToRadians } from "../utilities/index.js";
 const { QUERY_RADIUS_M, DB_ECHO_TTL } = process.env;
 
 export default (collection, {latitude, longitude}) => {
-  return collection.find(
-    {
+  return collection.find({
     $and: [
+      // Find only entries within the last ___TTL___ time period.
       { "timestamp": {$gt: new Date(Date.now() - (DB_ECHO_TTL * 1000))} },
       { "location": {
           $geoWithin: {
@@ -14,8 +14,7 @@ export default (collection, {latitude, longitude}) => {
         }
       }
     ]
-  }
-  ).toArray()
+  }).toArray()
     .then((locatedEchoes) => locatedEchoes.map(toClientFormattedEcho))
     .catch((err) => { console.error("Get Echoes Error", err) });
 };
